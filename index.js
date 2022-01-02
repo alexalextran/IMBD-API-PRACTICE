d05867f5a9527702a7a4153bf721387fb418efcb
alexalextran <alextran8385@gmail.com>
2022-01-02 11:16:29 +1100

    advanced search bar

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ const movieslist = document.querySelector(".movie__list")
 async function search(){
     const searchvalue = document.getElementById('searchBar').value
 
-    const movies = await fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=749ea3ae&s=${searchvalue}`)
+    const movies = await fetch(`http://www.omdbapi.com/?apikey=749ea3ae&s=${searchvalue}`)
     const moviesData = await movies.json()
 
     var hi = moviesData.Search.map((movie)=> movieHTML(movie)).join('')
 
-movieslist.innerHTML =hi
+movieslist.innerHTML = hi
 
 
 
@@ -19,12 +19,13 @@ movieslist.innerHTML =hi
 
 
 
+
 function movieHTML(movie){
     
 var movieobject =
     `<div class="movie">
-    <div class="img">
-    <img src = "${movie.Poster}" style="width:270px; height:400px">
+    <div>
+    <img class="poster" src = "${movie.Poster}" ">
     </div>
 
     <h3 class="movie__title">
