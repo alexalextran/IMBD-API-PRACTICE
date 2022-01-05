58a738257ba07009f5165534faa2e76d07f8f242
alexalextran <alextran8385@gmail.com>
2022-01-05 21:04:57 +1100

    next page is now works

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const movieslist = document.querySelector(".movie__list")
+const selector = document.querySelector(".page__selector")
 let error_notification = false
 let render_succsessful = false
+
 
 
 
@@ -9,22 +11,28 @@ async function search(event){
 
     
     event.preventDefault();
-    const searchvalue = document.getElementById('searchBar').value
-    const searchyear = document.getElementById('searchBar__year').value
-    var searchtype = document.getElementById("filter").value;
+
 
     if(render_succsessful == true){
         movieslist.innerHTML = "";
+        selector.innerHTML = ""
         render_succsessful = false
     }
 
 
-
-
 try{
-    const movies = await fetch(`http://www.omdbapi.com/?apikey=749ea3ae&s=${searchvalue}&y=${searchyear}&type=${searchtype}`)
-    const moviesData = await movies.json()
+    
+    const searchvalue = document.getElementById('searchBar').value
+const searchyear = document.getElementById('searchBar__year').value
+var searchtype = document.getElementById("filter").value;
+    var movies = await fetch(`http://www.omdbapi.com/?apikey=749ea3ae&s=${searchvalue}&y=${searchyear}&type=${searchtype}`)
+    var moviesData = await movies.json()
     console.log(error_notification)
+
+
+   
+    
+   
     var hi = moviesData.Search.map((movie)=> movieHTML(movie)).join('')
         
     
@@ -46,11 +54,19 @@ try{
         
                 }
 
+                
+
+                
+
 
 
 
 if(render_succsessful == false){
+
+
+
 movieslist.innerHTML = hi
+selector.innerHTML = page__selector()
 render_succsessful = true
 }
 
@@ -100,10 +116,8 @@ function movie_poster(poster){
 
 function movieHTML(movie){
 
+return  `<div class="movie" onclick="changeid('${movie.imdbID}')">
 
-    
-var movieobject =
-    `<div class="movie" onclick="changeid('${movie.imdbID}')">
     <div>
     ${movie_poster(movie.Poster)}
     </div>
@@ -121,8 +135,126 @@ var movieobject =
     </p>
 </div>`
 
-return movieobject
+
 }
+
+
+function page__selector(){
+    return `
+    
+    <a   id="prev" onclick="page__decrease()"> <<< Previous Page</a> <p  id="page__number">1</p> <a onclick="page__increase(event)" id="forw">Next Page >>></a>
+
+    `
+}
+
+var page = 1
+
+
+async function page__increase(event){
+    console.log('work')
+    
+    page++
+    
+    const page__number = document.getElementById('page__number')
+    page__number.innerHTML = page
+
+    const searchvalue = document.getElementById('searchBar').value
+    const searchyear = document.getElementById('searchBar__year').value
+    var searchtype = document.getElementById("filter").value;
+        const movies = await fetch(`http://www.omdbapi.com/?apikey=749ea3ae&s=${searchvalue}&y=${searchyear}&type=${searchtype}&page=${page}`)
+       
+        const moviesData = await movies.json()
+        const maxpage = (Math.ceil(moviesData.totalResults / 10))
+               console.log(maxpage)
+
+       
+       
+
+        if(page==maxpage){ 
+            document.getElementById('forw').style.cssText =
+                                        `
+                                        color: black;
+                                        text-decoration: none;
+                                        cursor: default;
+                                        pointer-events: none;
+                                        `
+
+        event.preventDefault()
+
+             
+        }
+        else{
+            document.getElementById('forw').style.cssText =
+            `
+            color: blue;
+            text-decoration: underline;
+            cursor: pointer;
+            `
+            
+        }
+            
+
+        var hi = moviesData.Search.map((movie)=> movieHTML(movie)).join('')
+        movieslist.innerHTML = hi
+}
+
+async function page__decrease(){
+    console.log('work')
+    page--
+    
+    const page__number = document.getElementById('page__number')
+    page__number.innerHTML = page
+
+    const searchvalue = document.getElementById('searchBar').value
+    const searchyear = document.getElementById('searchBar__year').value
+    var searchtype = document.getElementById("filter").value;
+        const movies = await fetch(`http://www.omdbapi.com/?apikey=749ea3ae&s=${searchvalue}&y=${searchyear}&type=${searchtype}&page=${page}`)
+       
+        const moviesData = await movies.json()
+        const maxpage = (Math.ceil(moviesData.totalResults / 10))
+               console.log(maxpage)
+
+       
+       
+
+        if(page==1){ 
+            document.getElementById('prev').style.cssText =
+                                        `
+                                        color: black;
+                                        text-decoration: none;
+                                        cursor: default;
+                                        `
+
+             
+        }
+        else{
+            document.getElementById('prev').style.cssText =
+                                        `
+                                        color: blue;
+                                        text-decoration: underline;
+                                        cursor: pointer;
+                                        `
+             document.getElementById('forw').style.cssText =
+                                        `
+                                        color: blue;
+                                        text-decoration: underline;
+                                        cursor: pointer;
+                                        `
+                                        
+
+            
+        }
+            
+
+        var hi = moviesData.Search.map((movie)=> movieHTML(movie)).join('')
+        movieslist.innerHTML = hi
+}
+
+
+
+
+
+
 
 
 function changeid(id){
