b88fdbfe6788649be5df05ca2a3ed12c1f461171
alexalextran <alextran8385@gmail.com>
2022-01-05 17:25:33 +1100

    now you can serch up multiple movies without having to refresh

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 const movieslist = document.querySelector(".movie__list")
 let error_notification = false
+let render_succsessful = false
 
 
 
 
 async function search(event){
+
+    
     event.preventDefault();
     const searchvalue = document.getElementById('searchBar').value
     const searchyear = document.getElementById('searchBar__year').value
     var searchtype = document.getElementById("filter").value;
+
+    if(render_succsessful == true){
+        movieslist.innerHTML = "";
+        render_succsessful = false
+    }
+
+
+
+
 try{
     const movies = await fetch(`http://www.omdbapi.com/?apikey=749ea3ae&s=${searchvalue}&y=${searchyear}&type=${searchtype}`)
     const moviesData = await movies.json()
@@ -37,8 +49,12 @@ try{
 
 
 
-
+if(render_succsessful == false){
 movieslist.innerHTML = hi
+render_succsessful = true
+}
+
+
 
 
             
